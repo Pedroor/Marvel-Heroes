ba7664b43e5aa1d311363a95d2be00c89b5cec60
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-03-19 12:48:32 -0300

    config: adding api response types

---
+++ src/types.ts
@@ -0,0 +1,72 @@
+export interface Hero {
+  readonly id: number;
+  readonly name: string;
+  readonly description: string;
+  readonly modified: Date;
+  readonly resourceURI: string;
+  readonly url: Url;
+  readonly thumbnail: Thumbnail;
+  readonly comics: Comics;
+  readonly stories: Stories;
+  readonly events: Events;
+  readonly series: Series;
+}
+
+interface Url {
+  readonly type: string;
+  readonly url: string;
+}
+
+interface Thumbnail {
+  readonly path: string;
+  readonly extension: string;
+}
+
+interface Comics {
+  readonly avaible: number;
+  readonly returned: number;
+  readonly collectionURI: string;
+  readonly items: [
+    {
+      readonly resourceURI: string;
+      readonly name: string;
+    }
+  ];
+}
+
+interface Stories {
+  readonly available: number;
+  readonly returned: number;
+  readonly collectionURI: string;
+  readonly items: [
+    {
+      readonly resourceURI: string;
+      readonly name: string;
+      readonly type: string;
+    }
+  ];
+}
+
+interface Events {
+  readonly available: number;
+  readonly returned: number;
+  readonly collectionURI: string;
+  readonly items: [
+    {
+      readonly resourceURI: string;
+      readonly name: string;
+    }
+  ];
+}
+
+interface Series {
+  readonly available: number;
+  readonly returned: number;
+  readonly collectionURI: string;
+  readonly items: [
+    {
+      resourceURI: string;
+      name: string;
+    }
+  ];
+}
